4393a4955702b6c6286c040ca53d8570dbe7693a
wujinjun-MC <qq1602647782@163.com>
2025-03-27 19:47:16 +0800

    Wait for remote debug exit if error

--- debug-via-reverse-proxy.sh
+++ debug-via-reverse-proxy.sh
@@ -1,4 +1,4 @@
-whoami
+echo "worker user name: $(whoami)"
 # cd /tmp
 # wget -q https://www.cpolar.com/static/downloads/releases/3.3.18/cpolar-stable-linux-amd64.zip -O cpolar.zip && unzip cpolar.zip
 if ! which cpolar
