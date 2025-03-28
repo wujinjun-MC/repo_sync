4dfbb67a36d73a66906c391803dcb76d9baebc94
wujinjun-MC <qq1602647782@163.com>
2025-03-28 10:06:30 +0800

    Fix create and split archive (6)

--- tar-and-split-command.sh
+++ tar-and-split-command.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tar --remove-files -czvf - --exclude=.git --exclude=.repo * .??* 2>/home/runner/repo_sync-tar.log | \
+tar --remove-files -czvf - * .??* 2>/home/runner/repo_sync-tar.log | \
     split -b "$SPLIT_SIZE" --numeric-suffixes=1 --filter='bash -c "tee >(md5sum > $FILE.md5) > $FILE"' - artifacts.tar.gz.
